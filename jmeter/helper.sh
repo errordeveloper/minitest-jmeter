4bd6cfa7ae99a45c2662886959ad8a99443717d5
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-08-16 18:09:38 +0000

    Misc improvements

--- jmeter/helper.sh
+++ jmeter/helper.sh
@@ -5,8 +5,22 @@ T="executable 'jmeter' should be in the path or defined as 'JMETER' environment 
 T="argument should be given with the path to a testplan file"
   (test -n "$1") && pass || fail
 
-next "running jmeter with '$1'"
+for j in $*
+do next "running jmeter with '$j'"
+  case `basename $j` in
+  bsf_basic.jmx)
 
-STR='1 - javascript: {"a":1,"b":2}'
-T="should ouput \"$STR\" 10 times"
-(test `$JMETER -n $JMETER_FLAGS -t $1 | grep "$STR" -c` = 10) && pass || fail
+    STR='1 - javascript: {"a":1,"b":2}'
+    T="should ouput \"$STR\" 2 times"
+      (test `$JMETER -n $JMETER_FLAGS -t $1 | grep "$STR" -c` = 2) && pass || fail
+
+    STR='^jruby 1.6.7.2'
+    T="should output JRuby version string 2 times"
+      (test `$JMETER -n $JMETER_FLAGS -t $1 | grep "$STR" -c` = 2) && pass || fail
+  ;;
+  *)
+    T="should write a test for '$j'"
+    skip
+  ;;
+  esac
+done
--- minitest.sh
+++ minitest.sh
@@ -3,13 +3,16 @@
 
 p=0
 f=0
+s=0
 
 step="$0"
 
-FAIL_MSG="\033[1;35m ✘ FAIL\033[1;0m"
-PASS_MSG="\033[1;32m ✔ PASS\033[1;0m"
-EXIT_MSG="\033[1;35m ✟ EXIT\033[1;0m"
-NEXT_MSG="\033[1;32m ➟ NEXT\033[1;0m"
+FAIL_MSG="\033[1;35m  ✘ FAIL\033[1;0m"
+PASS_MSG="\033[1;32m  ✔ PASS\033[1;0m"
+SKIP_MSG="\033[1;34m  ❉ SKIP\033[1;0m"
+EXIT_MSG="\033[1;35m  ✟ EXIT\033[1;0m"
+NEXT_MSG="\033[1;32m  ➟ NEXT\033[1;0m"
+DONE_MSG="\033[1;30m %s DONE\033[1;0m"
 
 function fail {
   printf "$FAIL_MSG ($step): $T!\n"; ((f++)); return 1;
@@ -19,11 +22,15 @@ function pass {
   printf "$PASS_MSG ($step): $T.\n"; ((p++)); return 0;
 }
 
+function skip {
+  printf "$SKIP_MSG ($step): $T!\n"; ((s++)); return 0;
+}
+
 function next {
   if [ ! $f = 0 ]
-  then printf "$EXIT_MSG ($step): [[$f failed, $p passed]] cannot proceed!\n";
+  then printf "$EXIT_MSG ($step): [[$f failed, $p passed, $s skipped]] cannot proceed!\n";
     exit 2;
-  else printf "$NEXT_MSG ($step): [[$f failed, $p passed]] will proceed.\n";
+  else printf "$NEXT_MSG ($step): [[$f failed, $p passed, $s skipped]] will proceed.\n";
     step="$1";
   fi
 }
@@ -36,3 +43,9 @@ T="should have a test to run"
 next "$this"
 
 source $this $*
+
+if [ ! $f = 0 ]
+then DONE_WTH=" ✘"
+else DONE_WTH=" ✔"
+fi
+printf "$DONE_MSG ➠ [[$f failed, $p passed, $s skipped]]\n" "$DONE_WTH"
